34c1c16ea449774270f6b37c805deb04014a5447
Rishith Reddy <babarishith@gmail.com>
2016-02-18 14:23:41 +0530

    basic schedule

--- controllers/schedule.py
+++ controllers/schedule.py
@@ -4,7 +4,18 @@ def index():
 	return dict(message="hello from schedule.py")
 
 def league():
-	num = int(request.vars.number)
+	if(request.vars.number):
+		num = int(request.vars.number)
+	else:
+		redirect(URL('index'))
+	if(request.vars.rinks):
+		rinks = int(request.vars.rinks)
+	else:
+		rinks = 1
+	if(request.vars.practice):
+		practice = True
+	else:
+		practice = False
 	rotation = ["Team" + str(i+1) for i in range(num)]
 
 	if num % 2:
@@ -18,5 +29,8 @@ def league():
 	for f in fixtures:    
 		for i in range(len(f)/2):
 			if "BYE" not in (f[i], f[num-i-1]):
+				if practice:
+					matches.append(tuple((f[i], "Practice")))
+					matches.append(tuple((f[num-i-1], "Practice")))
 				matches.append(tuple((f[i], f[num-i-1])))
 	return locals()
